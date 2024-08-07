f3aa7c4f97fdfee27fe442963b09367803ced760
codingbyahlv <code@ahlv.se>
2024-08-07 11:44:31 +0200

    update db and db models

--- WebApp/WebApp/Data/ApplicationDbContext.cs
+++ WebApp/WebApp/Data/ApplicationDbContext.cs
@@ -5,6 +5,5 @@ namespace WebApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-        public DbSet <AddressEntity> Addresses { get; set; }
     }
 }
--- WebApp/WebApp/Data/ApplicationUser.cs
+++ WebApp/WebApp/Data/ApplicationUser.cs
@@ -1,24 +1,25 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Data
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
+        public string? FirstName { get; set; } = null!;
+        public string? LastName { get; set; } = null!;
         public string? Biography { get; set; }
         public string? ProfileImage { get; set; } = "avatar.jpg";
-        public int? AddressId { get; set; }
-        public AddressEntity? Address { get; set; }
+        public string? AddressId { get; set; }
+        public UserAddress? Address { get; set; }
     }
 
-    public class AddressEntity
+    public class UserAddress
     {
-        public int Id { get; set; }
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string AddressLine1 { get; set; } = null!;
         public string? AddressLine2 { get; set; }
         public string PostalCode { get; set; } = null!;
         public string City { get; set; } = null!;
     }
-
 }
