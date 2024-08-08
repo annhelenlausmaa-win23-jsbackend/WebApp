624e25f0acc3c6e4a251f04f04b9f92795bb8b3f
codingbyahlv <code@ahlv.se>
2024-08-08 13:52:04 +0200

    implement notifications with subscriptions

---
+++ WebApp/WebApp/Models/SubscribeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models;
+
+public class SubscribeModel
+{
+    public string Email { get; set; } = null!;
+    public bool DailyNewsLetter { get; set; } = false;
+    public bool AdvertisingUpdates { get; set; } = false;
+    public bool WeekInReview { get; set; } = false;
+    public bool EventUpdates { get; set; } = false;
+    public bool StartupsWeekly { get; set; } = false;
+    public bool Podcasts { get; set; } = false;
+}
